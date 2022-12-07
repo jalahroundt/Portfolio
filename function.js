e914a83db1d58ae870546e3d4782048ad106b271
Jalah Roundtree <101207940+jalahroundt@users.noreply.github.com>
2022-12-06 19:09:03 -0500

    Add files via upload
    RWD Updates

---
+++ function.js
@@ -0,0 +1,48 @@
+function openOverlay1() {
+  document.getElementById("overlay1").style.width = "100%";
+}
+
+function closeOverlay1() {
+  document.getElementById("overlay1").style.width = "0%";
+}
+
+function openOverlay2() {
+  document.getElementById("overlay2").style.width = "100%";
+}
+
+function closeOverlay2() {
+  document.getElementById("overlay2").style.width = "0%";
+}
+
+function openOverlay3() {
+  document.getElementById("overlay3").style.width = "100%";
+}
+
+function closeOverlay3() {
+  document.getElementById("overlay3").style.width = "0%";
+}
+
+function openOverlay4() {
+  document.getElementById("overlay4").style.width = "100%";
+}
+
+function closeOverlay4() {
+  document.getElementById("overlay4").style.width = "0%";
+}
+
+function openOverlay5() {
+  document.getElementById("overlay5").style.width = "100%";
+}
+
+function closeOverlay5() {
+  document.getElementById("overlay5").style.width = "0%";
+}
+
+function openOverlay6() {
+  document.getElementById("overlay6").style.width = "100%";
+}
+
+function closeOverlay6() {
+  document.getElementById("overlay6").style.width = "0%";
+}
+
